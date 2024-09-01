d1c7426cb10365e4326144181e8dee46016d5638
Shaurya Tomar <shauryapst1@gmail.com>
2024-09-01 17:53:28 +0530

    [MODIFIED] - user interface [ADDED] - mongo id in session object and token.

--- auth.ts
+++ auth.ts
@@ -2,6 +2,9 @@ import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import Credentials from "next-auth/providers/credentials";
+import { User as NextAuthUser, Profile, Account } from "next-auth";
+import User from "./models/user";
+import { connectDB } from "./lib/mongoose";
 
 const providers = [
     // Credentials({
@@ -28,10 +31,50 @@ const providers = [
     GitHubProvider,
 ];
 
+interface UserInterface extends NextAuthUser{
+    userId ?: string
+}
+
 export const { handlers, auth, signIn, signOut } = NextAuth({
     pages: {
         signIn: "/login",
-        signOut: "/signout"
+        signOut: "/signout",
     },
     providers: providers,
+    callbacks: {
+        async signIn({ user, account }: { user: UserInterface, account:any }) {
+            try {
+                // Connect to the database
+                await connectDB();
+                // console.log(user);
+                // console.log(account);
+
+                // Check if the user exists in the database
+                let existingUser = await User.findOne({ email: user.email });
+
+                if (!existingUser) {
+                    existingUser =  await User.create({ email: user.email });
+                }
+                // console.log(existingUser._id.toString());
+                user.userId = existingUser._id.toString();
+                return true;
+            } catch (err) {
+                return false;
+            }
+        },
+        async session({ session, token }: { session: any, token: any }) {
+            // Attach MongoDB ID to the session object
+            if (token?.userId) {
+                session.user.userId = token.userId;
+            }
+            // console.log(session);
+            return session;
+        },
+        async jwt({ token, user }: { token: any, user?: UserInterface }) {
+            if (user) {
+                token.userId = user.userId;
+            }
+            return token;
+        },
+    },
 });
