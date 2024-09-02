70c4732b1bfbeef84b75bb3711d42120fed5ce77
Shaurya Tomar <shauryapst1@gmail.com>
2024-09-02 21:56:10 +0530

    [FIX] - use param in server comp

--- app/jobs/[jobid]/edit/page.tsx
+++ app/jobs/[jobid]/edit/page.tsx
@@ -1,11 +1,9 @@
-import { useParams } from "next/navigation";
+
 import { getJobData } from "../action";
 import JobForm from "../../create/JobForm";
 
 export default async function JobEditPage() {
-  const param = useParams();
-  const jobid = param.jobid;
-  const jobData = await getJobData(jobid.toString());
+
     return (
         <div className="m-5 flex items-center justify-center">
             <JobForm/>
